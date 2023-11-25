4665a44ba31654e94f2f557d0b5814a38791ddc7
Andrew Lawson <1607258+Boby360@users.noreply.github.com>
2023-11-25 00:41:39 -0800

    Update GC_watchdog.sh

--- GC_watchdog.sh
+++ GC_watchdog.sh
@@ -1,105 +1,203 @@
 #!/system/bin/sh
 #GC watchdog script
 
-ver='0.3.35'
-productmodel=`getprop ro.product.model`
-routerIP='192.168.0.1'
+#ver='0.3.35'
+productmodel=$(su -c "getprop ro.product.model")
+
+#Adjust for gateway
+routerIP=$(getprop net.dns1)
+
+
+#What percentage down of workers do we want before restarting?
+#This math doesn't math, but its close.
+percentage_down="50"
+
+
+#adjust these if adapters are different
+wifi_mac=$(cat /sys/class/net/wlan0/address) > /dev/null
+eth0_mac=$(cat /sys/class/net/eth0/address) > /dev/null
+
+
+echo "after startup stuff"
+sleep 1
 #To run at phone startup: 
 #su
 #rm /data/adb/service.d/*.sh
-#mv /sdcard/watchdog0.3.35.sh /data/adb/service.d/watchdog0.3.35.sh && chmod 777 /data/adb/service.d/watchdog0.3.35.sh && chown 0.0 /data/adb/service.d/watchdog0.3.35.sh
-
-while [ "$(getprop sys.boot_completed)" != 1 ];
-do sleep 1;
+#mv /sdcard/GC_watchdog.sh /data/adb/service.d/GC_watchdog.sh && chmod 777 /data/adb/service.d/GC_watchdog.sh && chown 0.0 /data/adb/service.d/GC_watchdog.sh
+echo "before while"
+while [ "$(getprop sys.boot_completed)" != 1 ]
+do sleep 1
 done
+
+
 sleep 2
+echo "after while"
 
-get_current_time() {
-    date +"%Y-%m-%d %H:%M:%S"
-}
 
-hostname = `cat /data/local/tmp/config.json | jq -r '.device_name'`
-workers_count = `cat config.json | jq -r '.workers_count'`
-su -c 'hostname '$hostname''
-su -c 'setprop net.hostname '$hostname''
-su -c 'setprop net.bt.name '$hostname''
-su -c 'settings put system device_name '$hostname''
-su -c 'settings put global device_name '$hostname''
-su -c 'setprop persist.usb.serialno '$hostname''
-su -c 'settings put secure bluetooth_name '$hostname''
-su -c 'settings put global synced_account_name '$hostname''
-
+echo "hostname command:"
+hostname=$(su -c "awk -F'\"' '/device_name/ {print \$4}' /data/local/tmp/config.json")
+echo "workers count command:"
+workers_count=$(su -c awk -F'[:,]' '/workers_count/ {gsub(/^[ \t]+/,"",$2); print $2}' /data/local/tmp/config.json)
+echo "$hostname"
+echo "This is my hostname"
+su -c "hostname $hostname"
+su -c "setprop net.hostname $hostname"
+echo "after hostnames"
+su -c "setprop net.bt.name $hostname"
+echo "some way through"
+su -c "settings put system device_name $hostname"
+su -c "settings put global device_name $hostname"
+echo "some more way through"
+su -c "setprop persist.usb.serialno $hostname"
+su -c "settings put secure bluetooth_name $hostname"
+su -c "settings put global synced_account_name $hostname"
+echo "start optimizations"
 #Optimizations:
 
-#whitelist pogo and launcher so we get all of the logcat errors.
-logcat -P "com.nianticlabs.pokemongo"
-logcat -P "com.gocheats.launcher"
+#Enable ADB, and make sure its on 5555:
+su -c "setprop service.adb.tcp.port 5555"
+#On my android 11 S6E, there was still some weirdness/problems
 
+#Supress any crash popups:
+su -c "settings put global anr_show_background_apps 0"
+
+#whitelist pogo and launcher so we get all of the logcat errors.
+su -c 'logcat -P ""'
+
+echo "set logcat whitelist"
+sleep 1
 
 #ATV optimizations
-pm disable com.google.android.tts
-pm disable com.droidlogic.BluetoothRemote
-pm disable com.google.android.apps.turbo
+pm disable com.google.android.tts > /dev/null
+pm disable com.droidlogic.BluetoothRemote > /dev/null
+pm disable com.google.android.apps.turbo > /dev/null
 
 
+######This is kind of risky
+
+#If ethernet has a valid IP address, disable WIFI completely.
+# su -c "ping -c1 -I wlan0 8.8.8.8" > /dev/null
+# if [ $? -eq 0 ]; then
+	# svc wifi disable
+# fi
+
+######This is kind of risky
+
+#We could look at this?
+#settings get global wifi_on 0/1
+
+if [[ -e "/sys/class/net/wlan0/address" && -e "/sys/class/net/eth0/address" ]]; then
+echo "Why...... does both wifi and eth have a mac?"
+echo "Disable one......"
+
+#lets assume eth0 is what we actually want
+mac=$eth0_mac
+fi
+
+#If wifi mac is empty, we use eth0 mac for our reference.
+if [[ ! -e "/sys/class/net/wlan0/address" && -e "/sys/class/net/eth0/address" ]]; then
+mac=$eth0_mac
+fi
+
+#if eth0 mac is empty, we use wifi mac as our reference.
+if [[ -e "/sys/class/net/wlan0/address" && ! -e "/sys/class/net/eth0/address" ]]; then
+mac=$wifi_mac
+fi
+
+#ATV A9:
+#Ethernet: /sys/class/net/eth0/address
+#Wifi: /sys/class/net/wlan0/address
+
+#For A7 ATVs this was used:
+# echo 1 > /sys/class/unifykeys/lock
+# echo mac > /sys/class/unifykeys/name
+# echo "$1" >/sys/class/unifykeys/write
+# cat /sys/class/unifykeys/read
+# echo 0 > /sys/class/unifykeys/lock
+
+
+if [[ $mac == "00:15:18:01:81:31" || $mac == "02:00:00:00:00:00" ]]; then
+
+new_mac=$(xxd -l 6 -p /dev/urandom |sed 's/../&:/g;s/:$//')
+
+su -c "ip link set dev wlan0 address $new_mac"
+or
+su -c "ifconfig wlan0 hw ether $new_mac"
+su -c "echo $new_mac > /efs/wifi/.mac.info"
+fi
+
+sleep 60
+#Lets see if GC is running.
+########Is GC running?
+if [[ $(pidof com.gocheats.launcher) == "" ]]
+then
+#Start GC
+monkey -p com.gocheats.launcher 1
+fi
+sleep 5
+
+echo "OOM"
 ##Keep things alive (OOM score)
 #Script
 rm /sdcard/pid-*.txt
 script_pid=`echo $$`
 echo $script_pid > /sdcard/pid-$script_pid.txt
-su -c 'echo -900 >> /proc/'$script_pid'/oom_score_adj'
+su -c "echo -900 >> /proc/$script_pid/oom_score_adj"
+echo "script done"
 
 #Launcher (Default when checked was 906)
-launcher_pid=`pidof com.gocheats.launcher`
+launcher_pid=$(pidof com.gocheats.launcher)
 echo $launcher_pid > /sdcard/pid-$launcher_pid.txt
 su -c 'echo -900 >> /proc/'$launcher_pid'/oom_score_adj'
 
 #Pogo default when checked was 700
 
-
+echo "Be nice"
 ##Lets be nice to the launcher
-su -c 'renice -n -15 -p $launcher_pid'
-su -c 'renice -n -14 -p $script_pid'
+su -c "renice -n -15 $launcher_pid"
+su -c "renice -n -14 $script_pid"
 
 ###############################Check Loop:
+echo "loop start"
 while true
 do
-
 #########Lets Grep the logcat and look for some stupid user error problems...
 DISCORD_WEBHOOK_URL="https://discord.com/api/webhooks/your-webhook-id/your-webhook-token"
 
 
+# Capture the logcat output
+log_output=$(logcat -d)
+
 # Check if the logcat contains a mismatch game version.
-if logcat -d | grep -q "Mismatching game version!"; then
-    LOG_LINE=$(logcat -d | grep "Mismatching game version!")
-    curl -X POST -H "Content-Type: application/json" -d '{"content": "Mismatching game version detected on $hostname:\n'"$LOG_LINE"'"}' "$DISCORD_WEBHOOK_URL"
-	sleep 300
-else
+if echo "$log_output" | grep -q "Mismatching game version!"; then
+    log_line=$(echo "$log_output" | grep "Mismatching game version!")
+    curl -X POST -H "Content-Type: application/json" -d '{"content": "Mismatching game version detected on '"$hostname"':\n'"$log_line"'"}' "$DISCORD_WEBHOOK_URL"
+    sleep 300
 fi
 
 # Check if the logcat contains a License validation error
-if logcat -d | grep -q "Mismatching game version!"; then
-    LOG_LINE=$(logcat -d | grep "License validation failed!")
+if echo "$log_output" | grep -q "License validation failed!"; then
+    log_line=$(echo "$log_output" | grep "License validation failed!")
     curl -X POST -H "Content-Type: application/json" -d '{"content": "License Validation error found on $hostname' "$DISCORD_WEBHOOK_URL"
 	sleep 300
-else
 fi
 
 
 sleep 1
 #########Verify ADB is running, ifnot, reset service.
-if busybox netstat -tuln | grep -q "5555"; then
+if netstat -tuln | awk '/5555/' > "/dev/null"; then
+echo "Online"
 else
 adb start-server
 fi
 
 sleep 1
 ########Is SELinux set to enforce after starting GC?
-if [[ $(pidof com.gocheats.launcher) != "" ]]
-then
-if [[ $(getenforce) == "Permissive" ]]
-then
-sleep 20
+if [[ $(pidof com.gocheats.launcher) != "" ]]; then
+echo "GC is running"
+if [[ $(getenforce) == "Permissive" ]]; then
+echo "Device is Permissive"
+sleep 5
 su -c 'setenforce 1'
 fi
 fi
@@ -107,15 +205,16 @@ fi
 
 sleep 1
 ########Are most of GC instances running?
-#If only 1 is running, then sleep and double check we aren't in the process of starting.
-if [[ $(netstat -t | grep -c 7070) < 2 ]] #we could use a percentage of $workers_count in the future
-then
+#worker_50percentage=$(expr $workers_count \* $percentage_down / 100)
+
+
+#This doesn't really math, as we will have 1 more connection for the MITM, seperate from the workers on 7070.
+if [[ $(netstat -t | grep -c 7070) < 2 ]]; then #we could use a percentage of $workers_count in the future
 sleep 120
 
 
 #If after sleep we are still only running 1 instance or less, then restart.
-if [[ $(netstat -t | grep -c 7070) < 2 ]]
-then
+if [[ $(netstat -t | grep -c 7070) < 2 ]]; then
 am force-stop com.nianticlabs.pokemongo
 am force-stop com.gocheats.launcher
 sleep 2
@@ -138,7 +237,8 @@ if [[ $(pidof com.gocheats.launcher) == "" ]]
 then
 
 #Grab log of crash
-logcat -d > /sdcard/logcat_GC_crash_$(get_current_time).txt
+time=$(date +"%Y-%m-%d %H:%M:%S")
+logcat -d > /sdcard/logcat_GC_crash_$time.txt
 
 am force-stop com.nianticlabs.pokemongo
 am force-stop com.gocheats.launcher
@@ -154,7 +254,7 @@ su -c 'renice -n -15 -p $launcher_pid'
 
 
 #Log that it crashed 
-echo "GC crashed at $(get_current_time)" >> /sdcard/GC_crashed.txt
+echo "GC crashed at $time" >> /sdcard/GC_crashed.txt
 
 #We pulled the logs for the crash. Clear up the logcat.
 su -c 'logcat -c'
@@ -170,7 +270,7 @@ sleep 1
 
 #>>>>>>Check if wifi is enabled prior to testing......
 
-ping -c1 $routerIP > /dev/null
+su -c "ping -c1 $routerIP" > /dev/null
 if [ $? -eq 0 ] #|| $? != 'connect: Network is unreachable'
 	then 
 		echo "Ping good"
@@ -180,7 +280,7 @@ if [ $? -eq 0 ] #|| $? != 'connect: Network is unreachable'
 		echo "Ping Fail" >> /sdcard/watchdog_pingfail.txt
 		#echo "The value:" $?
 		
-		if [ settings get global wifi_on == 1 ]
+		if [ $(settings get global wifi_on) == 1 ]
 		then
 		svc wifi disable
 		sleep 5;
@@ -193,6 +293,8 @@ if [ $? -eq 0 ] #|| $? != 'connect: Network is unreachable'
 		fi
 		
 fi
-#echo "Done all checks" #>> /sdcard/watchdog_success.txt
+echo "Done all checks"
 sleep 300
+
+#Need to use ctrl+C to kill, or kill process ID.
 done 
